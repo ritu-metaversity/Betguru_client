966572c2f7b362caf8ca97c0b1a0f98cdb77ee31
ritu-metaversity <rituraj@metaversitytechnologies.com>
2024-08-05 17:53:26 +0530

    casino header fixed

--- src/common/MobileHeader/MobileHeader.tsx
+++ src/common/MobileHeader/MobileHeader.tsx
@@ -6,13 +6,13 @@ import { useEffect, useState, type FC } from "react";
 import { listenToThemeChange } from "../../utils/themeEvent";
 import left from '../../Img/left.png'
 import { useNavigate, useParams } from "react-router-dom";
-interface Props{
-  hederName:string;
+interface Props {
+  hederName: string;
   userBalance: { balance: number; } | undefined
 }
 
 
-const MobileHeader:FC<Props> = ({hederName, userBalance}) => {
+const MobileHeader: FC<Props> = ({ hederName, userBalance }) => {
   const pathname = window.location.pathname.slice(1);
   const [themeColor, setThemeColor] = useState(localStorage.getItem("app-theme") || "purple-theme1");
 
@@ -22,9 +22,9 @@ const MobileHeader:FC<Props> = ({hederName, userBalance}) => {
     listenToThemeChange(setThemeColor);
   }, []);
 
-  const {id} = useParams();
+  const { id } = useParams();
 
-  const handleBack = ()=>{
+  const handleBack = () => {
     nav("/cricket");
   }
 
@@ -36,21 +36,25 @@ const MobileHeader:FC<Props> = ({hederName, userBalance}) => {
       <div className={`mob-bar-header-inn ${themeColor}`}>
         <div className="Binded-data">
           {
-           id && pathname.includes(id) && <img onClick={handleBack}  src={left} className="ng-star-inserted" alt="left-Icon" style={{
-              marginRight:"10px",
-              marginLeft:"6px"
-            }}/>
+            id && pathname.includes(id) && <img onClick={handleBack} src={left} className="ng-star-inserted" alt="left-Icon" style={{
+              marginRight: "10px",
+              marginLeft: "6px"
+            }} />
           }
-        
-          <h2 style={{
-            textTransform:"capitalize"
-          }}>{pathname === "cricket"?"Dashboard":pathname.includes("inplay")?hederName: pathname}</h2>
+          {
+            pathname?.includes("casino") ? <h2 style={{
+              textTransform: "capitalize"
+            }}>Casino</h2> :
+
+              <h2 style={{
+                textTransform: "capitalize"
+              }}>{pathname === "cricket" ? "Dashboard" : pathname.includes("inplay") ? hederName : pathname}</h2>}
         </div>
         <div className="img-text-right">
           <img src={walletPng} alt="Wallet" />
           <h6>
             {userBalance?.balance.toFixed(1)}
-            </h6>
+          </h6>
         </div>
       </div>
     </div>
